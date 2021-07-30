d1da6312b38acc6cdd0908f212f4a79f260c028f
guoxiangzhou <8188755@qq.com>
2021-07-30 23:44:15 +0800

    make login ui

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,154 +1,108 @@
 <template>
-  <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="8" :offset="8">
+        <div class="top-gap" />
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="8" :offset="8">
+        <el-card shadow="never">
+          <h1>欢迎登陆</h1>
+          <el-divider />
+          <el-form :model="loginForm" label-position="left">
+            <el-form-item label="邮箱" prop="username">
+              <el-input
+                v-model="loginForm.username"
+                placeholder="请输入邮箱地址"
+                type="text"
+                tabindex="1"
+                auto-complete="on"
+              />
+            </el-form-item>
 
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
-      </div>
+            <el-form-item label="密码" prop="password">
+              <el-input
+                v-model="loginForm.password"
+                type="password"
+                placeholder="请输入密码"
+                tabindex="2"
+                auto-complete="off"
+                show-password
+              />
+            </el-form-item>
 
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="请输入邮箱地址"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
+            <el-row>
+              <el-col :offset="4" :span="7">
+                <el-button type="primary" style="width: 100%" @click="handleLogin">登陆</el-button>
+              </el-col>
+              <el-col :offset="2" :span="7">
+                <el-button style="width: 100%" @click.native.prevent="showRegisterDlg">注册</el-button>
+              </el-col>
+            </el-row>
+          </el-form>
+        </el-card>
+      </el-col>
+    </el-row>
 
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="请输入密码"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
-      </el-form-item>
-
-      <el-row>
-        <el-col offset="4" span="7">
-          <el-button :loading="loading" type="primary" style="width:100%;" @click.native.prevent="handleLogin">登陆</el-button>
-        </el-col>
-        <el-col offset="2" span="7">
-          <el-button style="width:100%;" @click.native.prevent="handleRegister">注册</el-button>
-        </el-col>
-      </el-row>
-
-      <!-- <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div> -->
-
-    </el-form>
-
-    <el-dialog title="收货地址" :visible.sync="showRegisterDialog">
+    <el-dialog title="注册账户" width="40%" :close-on-click-modal="false" :visible.sync="showRegisterDialog">
       <el-form :model="registerForm">
-        <el-form-item prop="username">
-          <span class="svg-container">
-            <svg-icon icon-class="user" />
-          </span>
+        <el-form-item label="邮箱" prop="username">
           <el-input
-            ref="username"
             v-model="registerForm.username"
             placeholder="请输入邮箱地址"
-            name="username"
             type="text"
             tabindex="1"
             auto-complete="on"
           />
         </el-form-item>
 
-        <el-form-item prop="password">
-          <span class="svg-container">
-            <svg-icon icon-class="password" />
-          </span>
+        <el-form-item label="密码" prop="password">
           <el-input
-            :key="passwordType"
-            ref="password"
             v-model="registerForm.password"
-            :type="passwordType"
+            type="password"
             placeholder="请输入密码"
-            name="password"
             tabindex="2"
-            auto-complete="on"
-            @keyup.enter.native="handleLogin"
+            auto-complete="off"
+            show-password
           />
-          <span class="show-pwd" @click="showPwd">
-            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-          </span>
         </el-form-item>
 
-        <el-form-item class="verify-code" prop="username">
-          <el-input
-            ref="verifyCode"
-            v-model="registerForm.verifyCode"
-            placeholder="验证码"
-            name="verifyCode"
-            type="text"
-            tabindex="1"
-            auto-complete="on"
-          />
-          <span>
-            <el-button>获取验证码</el-button>
-          </span>
+        <el-form-item label="验证码" prop="verifyCode">
+          <el-row>
+            <el-col :span="6">
+              <el-input
+                v-model="registerForm.verifyCode"
+                placeholder="输入验证码"
+                type="text"
+                tabindex="3"
+                auto-complete="off"
+              />
+            </el-col>
+            <el-col :offset="1" :span="1">
+              <el-button @click="getVerifyCode">获取</el-button>
+            </el-col>
+          </el-row>
         </el-form-item>
-
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="showRegisterDialog = false">取 消</el-button>
-        <el-button type="primary" @click="showRegisterDialog = false">注 册</el-button>
+        <el-button type="primary" @click="handleRegister">注 册</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
 
 export default {
   name: 'Login',
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
-      } else {
-        callback()
-      }
-    }
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
-      } else {
-        callback()
-      }
-    }
     return {
       loginForm: {
         username: '',
         password: ''
       },
-      loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
-      },
-      loading: false,
-      passwordType: 'password',
       redirect: undefined,
 
       showRegisterDialog: false,
@@ -168,157 +122,42 @@ export default {
     }
   },
   methods: {
-    showPwd() {
-      if (this.passwordType === 'password') {
-        this.passwordType = ''
-      } else {
-        this.passwordType = 'password'
-      }
-      this.$nextTick(() => {
-        this.$refs.password.focus()
-      })
-    },
     handleLogin() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
+      console.log('登陆')
+      this.$store
+        .dispatch('user/login', this.loginForm)
+        .then(() => {
+          this.$router.push({ path: this.redirect || '/' })
+        })
+        .catch(() => {
+        })
+    },
+    showRegisterDlg() {
+      this.registerForm.username = ''
+      this.registerForm.password = ''
+      this.registerForm.verifyCode = ''
+      this.showRegisterDialog = true
+    },
+    getVerifyCode() {
+      this.$store
+        .dispatch('user/login', this.loginForm)
+        .then(() => {
+          console.log('获取验证码成功')
+        })
+        .catch(() => {
+          console.log('获取验证码失败')
+        })
     },
     handleRegister() {
-      console.log('this is handleRegister')
-      // this.$router.push({ path: '/register' })
-      this.showRegisterDialog = true
+      console.log('handle register')
     }
   }
 }
 </script>
 
-<style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
-
-$bg:#283443;
-$light_gray:#fff;
-$cursor: #fff;
-
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-  }
-}
-
-/* reset element-ui css */
-.login-container {
-
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
-    }
-  }
-
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
-  }
-
-  .verify-code {
-    border: 1px solid rgba(0, 0, 0, 0.1);
-    background: white;
-    border-radius: 5px;
-    color: #454545;
-    width: 20%;
-  }
-}
-</style>
-
-<style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
-
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-
-  // .tips {
-  //   font-size: 14px;
-  //   color: #fff;
-  //   margin-bottom: 10px;
-
-  //   span {
-  //     &:first-of-type {
-  //       margin-right: 16px;
-  //     }
-  //   }
-  // }
-
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-  }
-
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
+<style>
+.top-gap {
+  border-radius: 4px;
+  min-height: 200px;
 }
 </style>
