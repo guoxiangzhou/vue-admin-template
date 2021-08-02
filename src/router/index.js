fde0fb4f43fd0a6bba63747584fd0bdf6bff9726
xiangzhou.guo <xiangzhou.guo@zilliz.com>
2021-08-02 17:06:34 +0800

    modify

--- src/router/index.js
+++ src/router/index.js
@@ -51,7 +51,7 @@ export const constantRoutes = [
       path: 'dashboard',
       name: 'Dashboard',
       component: () => import('@/views/dashboard/index'),
-      meta: { title: 'Dashboard', icon: 'dashboard' }
+      meta: { title: '概览', icon: 'dashboard' }
     }]
   },
 
@@ -63,7 +63,7 @@ export const constantRoutes = [
         path: 'index',
         name: 'Games',
         component: () => import('@/views/games/index'),
-        meta: { title: 'Games', icon: 'games' }
+        meta: { title: '游戏', icon: 'el-icon-files' }
       }
     ]
   },
--- src/views/games/index.vue
+++ src/views/games/index.vue
@@ -1,45 +1,24 @@
 <template>
   <div>
-    <el-row>
-      <el-col :span="8" :offset="8">
-        <el-form label-position="left" label-width="80px" :model="formLabelAlign">
-          <el-form-item label="邮箱地址">
-            <el-input v-model="formLabelAlign.name" />
-          </el-form-item>
-          <el-form-item label="验证码">
-            <el-row>
-              <el-col span="17">
-                <el-input v-model="formLabelAlign.region" />
-              </el-col>
-              <el-col offset="1" span="4">
-                <el-button type="info">信息按钮</el-button>
-              </el-col>
-            </el-row>
-          </el-form-item>
-          <el-form-item label="密码">
-            <el-input v-model="formLabelAlign.type" />
-          </el-form-item>
-        </el-form>
-      </el-col>
-    </el-row>
-  </div>
-</template>
+    <div class="app-container">
+      <div class="filter-container">
+        <el-button class="filter-item" type="primary" icon="el-icon-edit" @click="handleCreate">
+          新游戏
+        </el-button>
+      </div>
+    </div>
+  </div></template>
 
 <script>
 export default {
   name: 'Register',
   data() {
     return {
-      formLabelAlign: {
-        name: 'xxx',
-        region: 'xxxx',
-        type: 'xxxx'
-      }
     }
   },
   methods: {
-    onSubmit() {
-      console.log('submit!')
+    handleCreate() {
+      console.log('handleCreate!')
     }
   }
 }
